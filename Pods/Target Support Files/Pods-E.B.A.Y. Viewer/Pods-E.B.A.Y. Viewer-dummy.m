ab71834be2848798d7f5056dd24e2f0b75787a37
Nikolay Unru <nikolay.unru@datcroft.com>
2019-01-25 21:47:20 +0400

    Install pods

---
+++ Pods/Target Support Files/Pods-E.B.A.Y. Viewer/Pods-E.B.A.Y. Viewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_E_B_A_Y__Viewer : NSObject
+@end
+@implementation PodsDummy_Pods_E_B_A_Y__Viewer
+@end
---
+++ Pods/Target Support Files/Pods-E.B.A.Y. ViewerTests/Pods-E.B.A.Y. ViewerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_E_B_A_Y__ViewerTests : NSObject
+@end
+@implementation PodsDummy_Pods_E_B_A_Y__ViewerTests
+@end
