8ba68d4d9cb05b878549bc9d6b07a4dbd147ea62
Nikolay Unru <nikolay.unru@datcroft.com>
2019-01-25 16:07:39 +0400

    Initial Commit

---
+++ E.B.A.Y. ViewerTests/E_B_A_Y__ViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  E_B_A_Y__ViewerTests.swift
+//  E.B.A.Y. ViewerTests
+//
+//  Created by NIKO on 25/01/2019.
+//  Copyright © 2019 NIKO. All rights reserved.
+//
+
+import XCTest
+@testable import E_B_A_Y__Viewer
+
+class E_B_A_Y__ViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
